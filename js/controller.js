f704291088599c878ab377bf247614a388640ef7
lioramar55 <lioramar55@gmail.com>
2022-02-16 17:49:04 +0200

    added drag text feature

--- js/controller.js
+++ js/controller.js
@@ -2,6 +2,10 @@
 
 var gCanvas
 var gCtx
+var gDrag = {
+  isOn: false,
+  startPos: {},
+}
 
 var gMeme = {
   selectedImgId: 5,
@@ -39,31 +43,62 @@ function addEditorListeners() {
   document.querySelector('.add').addEventListener('click', onAddLine)
   document.querySelector('.switch').addEventListener('click', onSwitchLine)
   document.querySelector('.trash').addEventListener('click', onDeleteLine)
+  document.querySelector('.increase-font').addEventListener('click', changeFontSize)
+  document.querySelector('.decrease-font').addEventListener('click', changeFontSize)
 }
 
 function addCanvasListeners() {
   gCanvas.addEventListener('click', onCanvasClick)
+  gCanvas.addEventListener('mousedown', startDrag)
+  gCanvas.addEventListener('mousemove', moveLine)
+  gCanvas.addEventListener('mouseup', stopDrag)
+}
+
+function startDrag(e) {
+  const x = e.offsetX
+  const y = e.offsetY
+  var selectedLine = isUserOnLine(x, y)
+  if (selectedLine !== -1) {
+    document.body.style.cursor = 'grabbing'
+    gDrag.isOn = true
+    gDrag.startPos = { x, y }
+  }
+}
+
+function moveLine(e) {
+  const x = e.offsetX
+  const y = e.offsetY
+  if (isUserOnLine(x, y) !== -1) document.body.style.cursor = 'grab'
+  else document.body.style.cursor = 'default'
+  if (!gDrag.isOn) return
+  var selectedLine = isUserOnLine(x, y)
+  if (selectedLine !== -1) {
+    gMeme.lines[selectedLine].x += x - gDrag.startPos.x
+    gMeme.lines[selectedLine].y += y - gDrag.startPos.y
+    gDrag.startPos = { x, y }
+    drawCanvas()
+  }
+}
+
+function stopDrag() {
+  gDrag.isOn = false
+}
+
+function changeFontSize(e) {
+  var isIncrease = e.path[1].classList.contains('increase-font')
+  if (isIncrease) gMeme.lines[gMeme.selectedLineIdx].size++
+  else gMeme.lines[gMeme.selectedLineIdx].size--
+  drawCanvas()
+  onSelectLine()
 }
 
 function onCanvasClick(e) {
   const x = e.offsetX
   const y = e.offsetY
-  let isLineSelected = false
-  let selectedLine
-  gMeme.lines.forEach((line, idx) => {
-    if (
-      x >= line.x &&
-      y >= line.y - 50 &&
-      x <= line.x + gCtx.measureText(line.txt).width &&
-      y < line.y + 50
-    ) {
-      isLineSelected = true
-      selectedLine = idx
-    }
-  })
-  if (isLineSelected) {
+  var selectedLine = isUserOnLine(x, y)
+  if (selectedLine !== -1) {
     gMeme.selectedLineIdx = selectedLine
-    onSelectLine()
+    onSelectLine(selectedLine)
   } else {
     drawCanvas()
   }
@@ -99,11 +134,27 @@ function onUserType(e) {
   gCtx.fillText(currLine.txt, currLine.x, currLine.y)
 }
 
-function onSelectLine() {
+function onSelectLine(selectedLine) {
+  if (selectedLine !== gMeme.lines[gMeme.selectedLineIdx]) drawCanvas()
   var currLine = gMeme.lines[gMeme.selectedLineIdx]
   if (!currLine || !currLine.txt) return
+  var lineHeight = gCtx.measureText(currLine.txt).fontBoundingBoxDescent
   var rectWidth = gCanvas.width * (8 / 10)
-  gCtx.strokeRect(20, currLine.y - 40, rectWidth, 60)
+  gCtx.strokeRect(gCanvas.width / 10, currLine.y - 50, rectWidth, currLine.y + lineHeight - 25)
+}
+
+function isUserOnLine(x, y) {
+  let isLineSelected = false
+  let selectedLine
+  gMeme.lines.forEach((line, idx) => {
+    var lineWidth = gCtx.measureText(line.txt).width
+    var lineHeight = gCtx.measureText(line.txt).fontBoundingBoxDescent
+    if (x >= line.x && y >= line.y - 50 && x <= line.x + lineWidth && y < line.y + lineHeight) {
+      isLineSelected = true
+      selectedLine = idx
+    }
+  })
+  return isLineSelected ? selectedLine : -1
 }
 
 function onSwitchLine() {
@@ -116,6 +167,7 @@ function onSwitchLine() {
 
 function onAddLine() {
   document.querySelector('.meme-editor input[type=text]').value = ''
+  var yCoord = gMeme.lines.length === 1 ? gCanvas.height - 100 : gCanvas.height / 2
   var newLine = {
     txt: '',
     size: 30,
@@ -123,7 +175,7 @@ function onAddLine() {
     color: 'black',
     font: 'Impact',
     x: 100,
-    y: 100 + gMeme.lines[gMeme.selectedLineIdx].y,
+    y: yCoord,
   }
   gMeme.selectedLineIdx++
   gMeme.lines.push(newLine)
@@ -132,6 +184,7 @@ function onAddLine() {
 function drawCanvas() {
   gCtx.drawImage(gMeme.selectedImg, 0, 0)
   gMeme.lines.forEach((line) => {
+    gCtx.font = line.size + 'px' + ' ' + line.font
     if (line.txt) gCtx.fillText(line.txt, line.x, line.y)
   })
 }
