51bb5db59e95b1e19526555af24a15f9d2e96b2e
lioramar55 <lioramar55@gmail.com>
2022-02-16 10:18:12 +0200

    first commit

---
+++ js/memesSentences.js
@@ -0,0 +1,17 @@
+const memesSentences = [
+  'I never eat falafel',
+  'DOMS DOMS EVERYWHERE',
+  'Stop Using i in for loops',
+  'Armed in knowledge',
+  'Js error "Unexpected String"',
+  'One does not simply write js',
+  'I`m a simple man i see vanilla JS, i click like!',
+  'JS, HTML,CSS?? Even my momma can do that',
+  'May the force be with you',
+  'I know JS',
+  'JS Where everything is made up and the rules dont matter',
+  'Not sure if im good at programming or good at googling',
+  'But if we could',
+  'JS what is this?',
+  'Write hello world , add to cv 7 years experienced',
+];
